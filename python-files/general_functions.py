eb28b0cc89f6fe435ba4c3fa440a0f23cdaf5866
Brandon Fan <brandonfan1256@gmail.com>
2017-12-08 23:04:08 -0500

    add pascal

--- python-files/general_functions.py
+++ python-files/general_functions.py
@@ -1,6 +1,20 @@
 def factorial(number):
     '''computes factorial'''
     result = 1
-    for i in range(1, number):
+    for i in range(1, number+ 1):
         result *= i
     return result
+
+def sumPascalRow(n):
+    return 2**n
+def nthPascalValue(n):
+    '''NOTE IMPORT FACTORIAL WITH THIS FUNCTION'''
+    numbers = 1
+    start = 1
+    while n > numbers:
+        start += 1
+        numbers += start
+    index = numbers - n
+    value = factorial(start - 1) / (factorial(index)
+                                    * factorial(start - 1 - index))
+    return value
